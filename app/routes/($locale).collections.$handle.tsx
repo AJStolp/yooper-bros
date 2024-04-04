d0482e455f2a0781a09cc4d0b54fb0b2ce4dd801
Anthony Stolp <anthony.stolp@gmail.com>
2024-04-04 15:39:30 -0400

    css change for categories

--- app/routes/($locale).collections.$handle.tsx
+++ app/routes/($locale).collections.$handle.tsx
@@ -63,7 +63,7 @@ export default function Collection() {
 
 function ProductsGrid({products}: {products: ProductItemFragment[]}) {
   return (
-    <div className="products-grid">
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
       {products.map((product, index) => {
         return (
           <ProductItem
